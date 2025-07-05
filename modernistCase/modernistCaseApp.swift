947bc1fbe5506ce61c08998e5a1b447884e14685
Mehmet TIRPAN <mehmettirpan92@gmail.com>
2025-07-05 13:36:56 +0200

    Initial Commit

---
+++ modernistCase/modernistCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  modernistCaseApp.swift
+//  modernistCase
+//
+//  Created by Mehmet Tırpan on 5.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct modernistCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
